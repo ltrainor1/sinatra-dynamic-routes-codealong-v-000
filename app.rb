7d13004bdbebfcab6ac3734ef3f60e0840f03d88
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-25 21:38:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-
+  attr_accessor :output
   # This is a sample static route.
   get '/hello' do
     "Hello World!"
@@ -14,5 +15,16 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @output = params[:num1] * params[:num2]
+    binding.pry
+    @output
+    binding.pry
+  end
 
 end
